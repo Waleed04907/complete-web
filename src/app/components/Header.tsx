0da1ee5fcca9e13fdbc10fd3c916d895111d66e6
waleed-saddique <wbs90704@gmail.com>
2024-09-05 19:40:35 +0500

    done

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import link from 'next/link'
+const Header = () => {
+  return (
+    <div>
+ <link href='\'>Home page </link>    
+ <link href='\about'>About page </link>  
+ <link href='\adress'>Adress page </link>   
+ <link href='\contact'>Contact page </link> 
+    </div>
+  )
+}
+
+export default Header
